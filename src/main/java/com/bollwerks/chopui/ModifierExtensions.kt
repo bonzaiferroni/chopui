740d5c78ec40e3624aad20a0c93cc4c36a108a78
Luke Bollwerk <lukas8@gmail.com>
2024-04-27 14:49:31 -0600

    modify FoodScreen

---
+++ src/main/java/com/bollwerks/chopui/ModifierExtensions.kt
@@ -0,0 +1,6 @@
+package com.bollwerks.chopui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+
+fun Modifier.addBasePadding() = this.padding(Constants.BASE_PADDING)
