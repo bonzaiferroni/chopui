13b1d4ffef9010e090015b5acc7fca8220c7d430
Luke Bollwerk <lukas8@gmail.com>
2024-05-29 15:27:10 -0600

    update to material3

--- src/main/java/streetlight/app/chopui/Scaffold.kt
+++ src/main/java/streetlight/app/chopui/Scaffold.kt
@@ -3,21 +3,23 @@ package streetlight.app.chopui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import cafe.adriel.voyager.navigator.Navigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Scaffold(
     title: String = "Ahoy Scaffold!",
@@ -41,7 +43,7 @@ fun Scaffold(
         { }
     }
 
-    val navIcon: @Composable (() -> Unit)? = if (navigator != null) {
+    val navIcon: @Composable () -> Unit = if (navigator != null) {
         {
             IconButton(onClick = { navigator.pop() }) {
                 Icon(
@@ -51,11 +53,16 @@ fun Scaffold(
             }
         }
     } else {
-        null
+        { }
     }
 
     Scaffold(
-        topBar = { TopAppBar(title = { Text(title) }, navigationIcon = navIcon) },
+        topBar = {
+            TopAppBar(
+                title = { Text(title) },
+                navigationIcon = navIcon
+            )
+        },
         floatingActionButton = fab,
         content = content,
         modifier = modifier
