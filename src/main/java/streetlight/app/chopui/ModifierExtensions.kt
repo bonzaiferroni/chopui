465f8bf05205c2fba5e0d467c89d62f77253a41f
Luke Bollwerk <lukas8@gmail.com>
2024-07-10 09:09:50 -0600

    Create Quest Profile

--- src/main/java/streetlight/app/chopui/ModifierExtensions.kt
+++ src/main/java/streetlight/app/chopui/ModifierExtensions.kt
@@ -2,5 +2,6 @@ package streetlight.app.chopui
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
+import streetlight.app.chopui.Constants.BASE_PADDING
 
-fun Modifier.addBasePadding() = this.padding(streetlight.app.chopui.Constants.BASE_PADDING)
+fun Modifier.addBasePadding() = this.padding(BASE_PADDING)
