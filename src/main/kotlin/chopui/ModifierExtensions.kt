29d3aa7ac2682999ac626493a2577ef6cd4159e1
Luke Bollwerk <lukas8@gmail.com>
2024-08-31 11:54:08 -0600

    fine

--- src/main/kotlin/chopui/ModifierExtensions.kt
+++
@@ -1,7 +0,0 @@
-package chopui
-
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
-import chopui.Constants.BASE_PADDING
-
-fun Modifier.addBasePadding() = this.padding(BASE_PADDING)
--- src/main/kotlin/chopui/extensions/DateTimeExtensions.kt
+++
@@ -1,7 +0,0 @@
-package chopui.extensions
-
-import kotlinx.datetime.Instant
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
-
-fun Instant.toLocalDateTime() = this.toLocalDateTime(TimeZone.currentSystemDefault())
